301e734dc39d256e7db25775ece176f8c93bda56
Miraç Doğan <miracdogn@gmail.com>
2024-01-13 21:39:20 +0300

    Switch players and sum total scores

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
 const diceEl = document.querySelector(".dice");
@@ -15,7 +17,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
+
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
 
 //Rolling the dice
 btnRoll.addEventListener("click", function () {
@@ -29,8 +41,22 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     //Add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     //Next player
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener("click", function () {
+  scores[activePlayer] += currentScore;
+
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  if (scores[activePlayer] >= 100) {
+  }
+
+  switchPlayer();
 });
